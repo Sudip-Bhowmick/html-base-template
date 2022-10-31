b2697d56365bac98d7b40c58bbc448340b979e03
Sudip Bhowmick Ting <writetosb786@gmail.com>
2022-11-01 00:56:15 +0530

    get code

--- public/js/global.js
+++ public/js/global.js
@@ -16,12 +16,35 @@ $( ".hamburger" ).on( "click", function() {
 });
 
 /* Current year */
-document.getElementById("current_year").innerHTML = new Date().getFullYear();
+// document.getElementById("current_year").innerHTML = new Date().getFullYear();
 
 /* banner slider */
-$(".bannerslider").slick({
-    dots: true,
-    infinite: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-});
+// $(".bannerslider").slick({
+//     dots: true,
+//     infinite: false,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+// });
+
+// copy text
+for (let i = 0; i < 10; i++) {
+    $( ".getcoderesult" ).each(function( index ) {
+        $(this).attr('id', i)
+        $('.copyimg').attr('onclick', 'CopyToClipboard(' + i + ')')
+    });
+}
+
+function CopyToClipboard(containerid) {
+  if (document.selection) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select().createTextRange();
+    document.execCommand("copy");
+  } else if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    alert("Text has been copied, now paste in the text-area")
+  }
+}
